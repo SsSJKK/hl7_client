8633525c5da377321955146bf02c4a6745b2854d
SsSJKK <kobil07@mail.ru>
2024-06-30 19:34:17 +0500

    -f

--- main.go
+++ main.go
@@ -28,12 +28,49 @@ type StorFile struct {
 	Close bool
 }
 
+var vsdata = `MSH|^~\\&|||||20240628210706||ORU^R01|804|P|2.3.1||||||UNICODE
+PID|1||ppid^^^^MR||фамишия^имя|||Муж
+PV1|1|Амбул. больной|^^место
+OBR|1||4|00001^Automated Count^99MRC||20240526123400|20240529133328|||||||20240526124500||||||||||HM||||||||1
+OBX|1|IS|08001^Take Mode^99MRC||O||||||F
+OBX|2|IS|08002^Blood Mode^99MRC||W||||||F
+OBX|3|IS|08003^Test Mode^99MRC||CBC+DIFF||||||F
+OBX|4|IS|01002^Ref Group^99MRC||Взрос.муж||||||F
+OBX|5|NM|30525-0^Age^LN||66|yr|||||F
+OBX|6|ST|01001^Remark^99MRC||комменнт||||||F
+OBX|7|NM|6690-2^WBC^LN||6.27|10*9/L|4.00-10.00|A|||F
+OBX|8|NM|704-7^BAS#^LN||0.02|10*9/L|0.00-0.10|A|||F
+OBX|9|NM|706-2^BAS%^LN||0.3|%|0.0-1.0|A|||F
+OBX|10|NM|751-8^NEU#^LN||3.94|10*9/L|2.00-7.00|A|||F
+OBX|11|NM|770-8^NEU%^LN||62.8|%|50.0-70.0|A|||F
+OBX|12|NM|711-2^EOS#^LN||0.39|10*9/L|0.02-0.50|A|||F
+OBX|13|NM|713-8^EOS%^LN||6.1|%|0.5-5.0|H~A|||F
+OBX|14|NM|731-0^LYM#^LN||1.61|10*9/L|0.80-4.00|A|||F
+OBX|15|NM|736-9^LYM%^LN||25.7|%|20.0-40.0|A|||F
+OBX|16|NM|742-7^MON#^LN||0.31|10*9/L|0.12-1.20|A|||F
+OBX|17|NM|5905-5^MON%^LN||5.1|%|3.0-12.0|A|||F
+OBX|18|NM|789-8^RBC^LN||4.35|10*12/L|4.00-5.50|A|||F
+OBX|19|NM|718-7^HGB^LN||125|g/L|120-160|A|||F
+OBX|20|NM|787-2^MCV^LN||84.4|fL|80.0-100.0|A|||F
+OBX|21|NM|785-6^MCH^LN||28.6|pg|27.0-34.0|A|||F
+OBX|22|NM|786-4^MCHC^LN||339|g/L|320-360|A|||F
+OBX|23|NM|788-0^RDW-CV^LN||12.5|%|11.0-16.0|A|||F
+OBX|24|NM|21000-5^RDW-SD^LN||41.7|fL|35.0-56.0|A|||F
+OBX|25|NM|4544-3^HCT^LN||36.8|%|40.0-54.0|L~A|||F
+OBX|26|NM|777-3^PLT^LN||259|10*9/L|100-300|A|||F
+OBX|27|NM|32623-1^MPV^LN||9.0|fL|6.5-12.0|A|||F
+OBX|28|NM|32207-3^PDW^LN||15.9||9.0-17.0|A|||F
+OBX|29|NM|10002^PCT^99MRC||0.234|%|0.108-0.282|A|||F
+OBX|30|NM|10013^PLCC^99MRC||52|10*9/L|30-90|A|||F
+OBX|31|NM|10014^PLCR^99MRC||20.2|%|11.0-45.0|A|||F`
+
 func newStorFile() (*StorFile, error) {
 	f, err := os.OpenFile(fmt.Sprintf("%d.csv", time.Now().Unix()), os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		logging.Error("Error:", slog.String("->", err.Error()))
 		return nil, err
 	}
+	f.WriteString("ID пробы,Имя,Фамилия,Режим,Дата,Время,Состоян.пробы,WBC (10^9/L),Neu# (10^9/L),Lym# (10^9/L),Mon# (10^9/L),Eos# (10^9/L),Bas# (10^9/L),Neu% (%),Lym% (%),Mon% (%),Eos% (%),Bas% (%),RBC (10^12/L),HGB (g/L),HCT (%),MCV (fL),MCH (pg),MCHC (g/L),RDW-CV (%),RDW-SD (fL),PLT (10^9/L),MPV (fL),PDW ( ),PCT (%),P-LCC (10^9/L),P-LCR (%),ID пациента,Пол,Тип пациента,Конт.группа,Дата рождения,Возраст,Отделение,Место №,Дата отбора,Время отбора,Дата доставки,Время доставки,Врач,Оператор,Проверил(а),Комментарии,Сообщ.о WBC,Сообщ.о RBC,Сообщ.о PLT,Группа крови,РОЭ,Микроскопич.парам.\r\n")
 	return &StorFile{
 		F:     f,
 		Close: false,
@@ -44,15 +81,15 @@ func init() {
 	flag.StringVar(&startType, "type", "", "server or client")
 	flag.StringVar(&host, "host", "", "server or client")
 	flag.Parse()
-	if startType == "" {
-		fmt.Println("type is required")
-		os.Exit(1)
-	}
+	// if startType == "" {
+	// 	fmt.Println("type is required")
+	// 	os.Exit(1)
+	// }
 
-	if host == "" {
-		fmt.Println("host is required")
-		os.Exit(1)
-	}
+	// if host == "" {
+	// 	fmt.Println("host is required")
+	// 	os.Exit(1)
+	// }
 
 	logging = *slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
@@ -74,6 +111,7 @@ func main() {
 			<-time.After(time.Second * 10)
 		}
 	default:
+		decoder([]byte(vsdata))
 		fmt.Println("type is invalid")
 	}
 
