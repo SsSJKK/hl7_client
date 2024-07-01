04e31021cafbf7313b4e6f4475f277ff23c22d37
SsSJKK <kobil07@mail.ru>
2024-07-01 22:26:57 +0500

    -f

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"log/slog"
 	"net"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/kardianos/hl7"
@@ -28,41 +29,72 @@ type StorFile struct {
 	Close bool
 }
 
-var vsdata = `MSH|^~\\&|||||20240628210706||ORU^R01|804|P|2.3.1||||||UNICODE
-PID|1||ppid^^^^MR||фамишия^имя|||Муж
-PV1|1|Амбул. больной|^^место
-OBR|1||4|00001^Automated Count^99MRC||20240526123400|20240529133328|||||||20240526124500||||||||||HM||||||||1
+var vsdata = `MSH|^~\\&|||||20240701145039||ORU^R01|35|P|2.3.1||||||UNICODE
+PID|1||^^^^MR
+PV1|1
+OBR|1||16|00001^Automated Count^99MRC|||20240701104947|||||||||||||||||HM||||||||1
 OBX|1|IS|08001^Take Mode^99MRC||O||||||F
 OBX|2|IS|08002^Blood Mode^99MRC||W||||||F
-OBX|3|IS|08003^Test Mode^99MRC||CBC+DIFF||||||F
-OBX|4|IS|01002^Ref Group^99MRC||Взрос.муж||||||F
-OBX|5|NM|30525-0^Age^LN||66|yr|||||F
-OBX|6|ST|01001^Remark^99MRC||комменнт||||||F
-OBX|7|NM|6690-2^WBC^LN||6.27|10*9/L|4.00-10.00|A|||F
-OBX|8|NM|704-7^BAS#^LN||0.02|10*9/L|0.00-0.10|A|||F
-OBX|9|NM|706-2^BAS%^LN||0.3|%|0.0-1.0|A|||F
-OBX|10|NM|751-8^NEU#^LN||3.94|10*9/L|2.00-7.00|A|||F
-OBX|11|NM|770-8^NEU%^LN||62.8|%|50.0-70.0|A|||F
-OBX|12|NM|711-2^EOS#^LN||0.39|10*9/L|0.02-0.50|A|||F
-OBX|13|NM|713-8^EOS%^LN||6.1|%|0.5-5.0|H~A|||F
-OBX|14|NM|731-0^LYM#^LN||1.61|10*9/L|0.80-4.00|A|||F
-OBX|15|NM|736-9^LYM%^LN||25.7|%|20.0-40.0|A|||F
-OBX|16|NM|742-7^MON#^LN||0.31|10*9/L|0.12-1.20|A|||F
-OBX|17|NM|5905-5^MON%^LN||5.1|%|3.0-12.0|A|||F
-OBX|18|NM|789-8^RBC^LN||4.35|10*12/L|4.00-5.50|A|||F
-OBX|19|NM|718-7^HGB^LN||125|g/L|120-160|A|||F
-OBX|20|NM|787-2^MCV^LN||84.4|fL|80.0-100.0|A|||F
-OBX|21|NM|785-6^MCH^LN||28.6|pg|27.0-34.0|A|||F
-OBX|22|NM|786-4^MCHC^LN||339|g/L|320-360|A|||F
-OBX|23|NM|788-0^RDW-CV^LN||12.5|%|11.0-16.0|A|||F
-OBX|24|NM|21000-5^RDW-SD^LN||41.7|fL|35.0-56.0|A|||F
-OBX|25|NM|4544-3^HCT^LN||36.8|%|40.0-54.0|L~A|||F
-OBX|26|NM|777-3^PLT^LN||259|10*9/L|100-300|A|||F
-OBX|27|NM|32623-1^MPV^LN||9.0|fL|6.5-12.0|A|||F
-OBX|28|NM|32207-3^PDW^LN||15.9||9.0-17.0|A|||F
-OBX|29|NM|10002^PCT^99MRC||0.234|%|0.108-0.282|A|||F
-OBX|30|NM|10013^PLCC^99MRC||52|10*9/L|30-90|A|||F
-OBX|31|NM|10014^PLCR^99MRC||20.2|%|11.0-45.0|A|||F`
+OBX|3|IS|01002^Ref Group^99MRC||Общая||||||F
+OBX|4|NM|6690-2^WBC^LN||5.5|10*9/L|4.0-10.0|N|||F
+OBX|5|NM|731-0^LYM#^LN||2.7|10*9/L|0.8-4.0|N|||F
+OBX|6|NM|736-9^LYM%^LN||48.3|%|20.0-40.0|H~N|||F
+OBX|7|NM|789-8^RBC^LN||4.29|10*12/L|3.50-5.50|N|||F
+OBX|8|NM|718-7^HGB^LN||118|g/L|110-160|N|||F
+OBX|9|NM|787-2^MCV^LN||73.3|fL|80.0-100.0|L~N|||F
+OBX|10|NM|785-6^MCH^LN||27.5|pg|27.0-34.0|N|||F
+OBX|11|NM|786-4^MCHC^LN||375|g/L|320-360|H~N|||F
+OBX|12|NM|788-0^RDW-CV^LN||11.6|%|11.0-16.0|N|||F
+OBX|13|NM|21000-5^RDW-SD^LN||31.8|fL|35.0-56.0|L~N|||F
+OBX|14|NM|4544-3^HCT^LN||31.4|%|37.0-54.0|L~N|||F
+OBX|15|NM|777-3^PLT^LN||310|10*9/L|180-320|N|||F
+OBX|16|NM|32623-1^MPV^LN||9.2|fL|6.5-12.0|N|||F
+OBX|17|NM|32207-3^PDW^LN||15.8||15.0-17.0|N|||F
+OBX|18|NM|10002^PCT^99MRC||0.286|%|0.108-0.282|H~N|||F
+OBX|19|NM|10027^MID#^99MRC||0.6|10*9/L|0.1-1.5|N|||F
+OBX|20|NM|10029^MID%^99MRC||10.6|%|3.0-15.0|N|||F
+OBX|21|NM|10028^GRAN#^99MRC||2.2|10*9/L|2.0-7.0|N|||F
+OBX|22|NM|10030^GRAN%^99MRC||41.1|%|50.0-70.0|L~N|||F`
+// var vsdata = `MSH|^~\\&|||||20240628210706||ORU^R01|804|P|2.3.1||||||UNICODE
+// PID|1||ppid^^^^MR||фамишия^имя|||Муж
+// PV1|1|Амбул. больной|^^место
+// OBR|1||4|00001^Automated Count^99MRC||20240526123400|20240529133328|||||||20240526124500||||||||||HM||||||||1
+// OBX|1|IS|08001^Take Mode^99MRC||O||||||F
+// OBX|2|IS|08002^Blood Mode^99MRC||W||||||F
+// OBX|3|IS|08003^Test Mode^99MRC||CBC+DIFF||||||F
+// OBX|4|IS|01002^Ref Group^99MRC||Взрос.муж||||||F
+// OBX|5|NM|30525-0^Age^LN||66|yr|||||F
+// OBX|6|ST|01001^Remark^99MRC||комменнт||||||F
+// OBX|7|NM|6690-2^WBC^LN||6.27|10*9/L|4.00-10.00|A|||F
+// OBX|8|NM|704-7^BAS#^LN||0.02|10*9/L|0.00-0.10|A|||F
+// OBX|9|NM|706-2^BAS%^LN||0.3|%|0.0-1.0|A|||F
+// OBX|10|NM|751-8^NEU#^LN||3.94|10*9/L|2.00-7.00|A|||F
+// OBX|11|NM|770-8^NEU%^LN||62.8|%|50.0-70.0|A|||F
+// OBX|12|NM|711-2^EOS#^LN||0.39|10*9/L|0.02-0.50|A|||F
+// OBX|13|NM|713-8^EOS%^LN||6.1|%|0.5-5.0|H~A|||F
+// OBX|14|NM|731-0^LYM#^LN||1.61|10*9/L|0.80-4.00|A|||F
+// OBX|15|NM|736-9^LYM%^LN||25.7|%|20.0-40.0|A|||F
+// OBX|16|NM|742-7^MON#^LN||0.31|10*9/L|0.12-1.20|A|||F
+// OBX|17|NM|5905-5^MON%^LN||5.1|%|3.0-12.0|A|||F
+// OBX|18|NM|789-8^RBC^LN||4.35|10*12/L|4.00-5.50|A|||F
+// OBX|19|NM|718-7^HGB^LN||125|g/L|120-160|A|||F
+// OBX|20|NM|787-2^MCV^LN||84.4|fL|80.0-100.0|A|||F
+// OBX|21|NM|785-6^MCH^LN||28.6|pg|27.0-34.0|A|||F
+// OBX|22|NM|786-4^MCHC^LN||339|g/L|320-360|A|||F
+// OBX|23|NM|788-0^RDW-CV^LN||12.5|%|11.0-16.0|A|||F
+// OBX|24|NM|21000-5^RDW-SD^LN||41.7|fL|35.0-56.0|A|||F
+// OBX|25|NM|4544-3^HCT^LN||36.8|%|40.0-54.0|L~A|||F
+// OBX|26|NM|777-3^PLT^LN||259|10*9/L|100-300|A|||F
+// OBX|27|NM|32623-1^MPV^LN||9.0|fL|6.5-12.0|A|||F
+// OBX|28|NM|32207-3^PDW^LN||15.9||9.0-17.0|A|||F
+// OBX|29|NM|10002^PCT^99MRC||0.234|%|0.108-0.282|A|||F
+// OBX|30|NM|10013^PLCC^99MRC||52|10*9/L|30-90|A|||F
+// OBX|31|NM|10014^PLCR^99MRC||20.2|%|11.0-45.0|A|||F`
+
+var (
+	header = "ID пробы,Имя,Фамилия,Режим,Дата,Время,Состоян.пробы,WBC (10^9/L),Neu# (10^9/L),Lym# (10^9/L),Mon# (10^9/L),Eos# (10^9/L),Bas# (10^9/L),Neu% (%),Lym% (%),Mon% (%),Eos% (%),Bas% (%),RBC (10^12/L),HGB (g/L),HCT (%),MCV (fL),MCH (pg),MCHC (g/L),RDW-CV (%),RDW-SD (fL),PLT (10^9/L),MPV (fL),PDW ( ),PCT (%),P-LCC (10^9/L),P-LCR (%),ID пациента,Пол,Тип пациента,Конт.группа,Дата рождения,Возраст,Отделение,Место №,Дата отбора,Время отбора,Дата доставки,Время доставки,Врач,Оператор,Проверил(а),Комментарии,Сообщ.о WBC,Сообщ.о RBC,Сообщ.о PLT,Группа крови,РОЭ,Микроскопич.парам."
+	layout = `"EntityIdentifier","LNAME",WB-CD,"FNAME","DATE",,"TIME","WBC","NEU#","LYM#","MON#","EOS#","BAS#","NEU%","LYM%","MON%","EOS%","BAS%","RBC","HGB","HCT","MCV","MCH","MCHC","RDW-CV","RDW-SD","PLT","MPV","PDW","PCT","PLCC","PLCR","PIID","SEX","PCLASS","Ref Group","BDATE","Age",,,,,,,,,,,,,,,,`
+)
 
 func newStorFile() (*StorFile, error) {
 	f, err := os.OpenFile(fmt.Sprintf("%d.csv", time.Now().Unix()), os.O_CREATE|os.O_WRONLY, 0644)
@@ -70,7 +102,7 @@ func newStorFile() (*StorFile, error) {
 		logging.Error("Error:", slog.String("->", err.Error()))
 		return nil, err
 	}
-	f.WriteString("ID пробы,Имя,Фамилия,Режим,Дата,Время,Состоян.пробы,WBC (10^9/L),Neu# (10^9/L),Lym# (10^9/L),Mon# (10^9/L),Eos# (10^9/L),Bas# (10^9/L),Neu% (%),Lym% (%),Mon% (%),Eos% (%),Bas% (%),RBC (10^12/L),HGB (g/L),HCT (%),MCV (fL),MCH (pg),MCHC (g/L),RDW-CV (%),RDW-SD (fL),PLT (10^9/L),MPV (fL),PDW ( ),PCT (%),P-LCC (10^9/L),P-LCR (%),ID пациента,Пол,Тип пациента,Конт.группа,Дата рождения,Возраст,Отделение,Место №,Дата отбора,Время отбора,Дата доставки,Время доставки,Врач,Оператор,Проверил(а),Комментарии,Сообщ.о WBC,Сообщ.о RBC,Сообщ.о PLT,Группа крови,РОЭ,Микроскопич.парам.\r\n")
+	f.WriteString(header)
 	return &StorFile{
 		F:     f,
 		Close: false,
@@ -81,6 +113,7 @@ func init() {
 	flag.StringVar(&startType, "type", "", "server or client")
 	flag.StringVar(&host, "host", "", "server or client")
 	flag.Parse()
+
 	// if startType == "" {
 	// 	fmt.Println("type is required")
 	// 	os.Exit(1)
@@ -98,6 +131,38 @@ func init() {
 	}))
 	fileStore = &StorFile{
 		Close: true,
+	}
+	confRead()
+}
+
+func confRead() {
+	fConf, err := os.Open("config")
+	if err != nil {
+		logging.Error("Error:", slog.String("->", err.Error()))
+		return
+	}
+	defer fConf.Close()
+	dataConf, err := io.ReadAll(fConf)
+	if err != nil {
+		logging.Error("Error:", slog.String("->", err.Error()))
+		return
+	}
+	dataConf = bytes.ReplaceAll(dataConf, []byte("\r\n"), []byte("\n"))
+	rData := bytes.Split(dataConf, []byte("\n"))
+	vals := make(map[string]string)
+	for _, v := range rData {
+		vv := bytes.Split(v, []byte("="))
+		if len(vv) != 2 {
+			continue
+		}
+		vals[string(vv[0])] = string(vv[1])
+	}
+
+	if v, ok := vals["header"]; ok && v != "" {
+		header = v
+	}
+	if v, ok := vals["layout"]; ok && v != "" {
+		layout = v
 	}
 }
 
@@ -271,44 +336,54 @@ func decoder(data []byte) {
 func writeToFile(vals map[string]string) {
 	var err error
 	// fmt.Printf("vals: %v\n", vals)
-	daata := fmt.Sprintf(`"%s","%s","%s",WB-CD,"%s","%s",,"%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s",,,,,,,,,,,,,,,,`,
-		vals["EntityIdentifier"],
-		vals["LNAME"],
-		vals["FNAME"],
-		vals["DATE"],
-		vals["TIME"],
-		vals["WBC"],
-		vals["NEU#"],
-		vals["LYM#"],
-		vals["MON#"],
-		vals["EOS#"],
-		vals["BAS#"],
-		vals["NEU%"],
-		vals["LYM%"],
-		vals["MON%"],
-		vals["EOS%"],
-		vals["BAS%"],
-		vals["RBC"],
-		vals["HGB"],
-		vals["HCT"],
-		vals["MCV"],
-		vals["MCH"],
-		vals["MCHC"],
-		vals["RDW-CV"],
-		vals["RDW-SD"],
-		vals["PLT"],
-		vals["MPV"],
-		vals["PDW"],
-		vals["PCT"],
-		vals["PLCC"],
-		vals["PLCR"],
-		vals["PIID"],
-		vals["SEX"],
-		vals["PCLASS"],
-		vals["Ref Group"],
-		vals["BDATE"],
-		vals["Age"],
-	)
+	// data := fmt.Sprintf(`"%s","%s","%s",WB-CD,"%s","%s",,"%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s",,,,,,,,,,,,,,,,`,
+	// 	vals["EntityIdentifier"],
+	// 	vals["LNAME"],
+	// 	vals["FNAME"],
+	// 	vals["DATE"],
+	// 	vals["TIME"],
+	// 	vals["WBC"],
+	// 	vals["NEU#"],
+	// 	vals["LYM#"],
+	// 	vals["MON#"],
+	// 	vals["EOS#"],
+	// 	vals["BAS#"],
+	// 	vals["NEU%"],
+	// 	vals["LYM%"],
+	// 	vals["MON%"],
+	// 	vals["EOS%"],
+	// 	vals["BAS%"],
+	// 	vals["RBC"],
+	// 	vals["HGB"],
+	// 	vals["HCT"],
+	// 	vals["MCV"],
+	// 	vals["MCH"],
+	// 	vals["MCHC"],
+	// 	vals["RDW-CV"],
+	// 	vals["RDW-SD"],
+	// 	vals["PLT"],
+	// 	vals["MPV"],
+	// 	vals["PDW"],
+	// 	vals["PCT"],
+	// 	vals["PLCC"],
+	// 	vals["PLCR"],
+	// 	vals["PIID"],
+	// 	vals["SEX"],
+	// 	vals["PCLASS"],
+	// 	vals["Ref Group"],
+	// 	vals["BDATE"],
+	// 	vals["Age"],
+	// )
+	data := layout
+	layoutC := strings.ReplaceAll(layout, `"`, "")
+	for _, v := range strings.Split(layoutC, ",") {
+		if v == "" {
+			continue
+		}
+		if val, ok := vals[v]; ok {
+			data = strings.Replace(data, v, val, 1)
+		}
+	}
 	if fileStore.Close {
 		fileStore.Close = false
 		fileStore, err = newStorFile()
@@ -317,6 +392,6 @@ func writeToFile(vals map[string]string) {
 			return
 		}
 	}
-	fileStore.F.Write([]byte(daata))
 	fileStore.F.Write([]byte("\r\n"))
+	fileStore.F.Write([]byte(data))
 }
